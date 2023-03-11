ba95e2c10d8b7ab19f76c5c9b5f93ecba722adcc
Raphael Pour <info@raphaelpour.de>
2023-03-11 10:42:44 +0100

    babystep: convert macros to procedures

--- tutorials/babystep/boot.asm
+++ tutorials/babystep/boot.asm
@@ -5,37 +5,42 @@
 %define EOF 0
 %define BG_COLOR 9 ; royal blue
 
-%macro Print 1 ; define macro with one argument
-   mov si, word %1 ; push argument to si
-next_char:lodsb    ; load next char of string in si
+[ORG 0x7c00]
+   xor ax, ax ; clear ax by xoring it with itself
+   mov ds, ax ; move ax to the datasegment register
+   cld        ; clear direction flag, that decides in which direction
+              ; e.g. loadsb should load the memory (++ as usual or -- for
+			  ; reading backwards)
+
+   mov bl, BG_COLOR  ; store the background color in the proper register
+                     ; for int10h with function 0xB
+   call set_bg_color
+
+   mov si, msg  ; store msg pointer into the proper register for the
+                ; int10h with function 0xE
+   call print
+
+hang:
+   jmp hang
+ 
+set_bg_color:
+   mov ah, 0x0B    ; select 'set background' function
+   mov bh, 0x0     ; select 'set background' function
+   int 0x10        ; call interrupt
+   ret
+
+print:
+   lodsb           ; load next byte from 
    or al, al       ; check if null terminator reached
    jz done         ; finish print method if null terminator reached
    mov bl, 1
    mov ah, 0x0E    ; select 'Write character in TTY Mode' function of int 10h 
    int 0x10        ; call interrupt
-   jmp next_char   ; process next char
+   jmp print       ; process next char
 done:
-%endmacro
+   ret
 
-%macro SetBackgroundColor 1
-   mov ah, 0x0B    ; select 'set background' function
-   mov bh, 0x0     ; select 'set background' function
-   mov bl, %1      ; select color
-   int 0x10        ; call interrupt
-%endmacro
-
-[ORG 0x7c00]
-   xor ax, ax
-   mov ds, ax
-   cld
-
-   SetBackgroundColor BG_COLOR
-   Print msg
-
-hang:
-   jmp hang
- 
-msg   db 'Hello Raphael', CR, LF, EOF
+msg   db 'Hello Raphael!', CR, LF, EOF
  
    times 510-($-$$) db 0
    db 0x55
