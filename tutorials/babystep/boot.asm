b6510642f2ba0cecce9e2de072dcae90589bc5b1
Raphael Pour <info@raphaelpour.de>
2023-03-13 21:28:08 +0100

    babystep: refactoring

--- tutorials/babystep/boot.asm
+++ tutorials/babystep/boot.asm
@@ -1,8 +1,8 @@
 ; nasm reference: https://www.nasm.us/doc/nasmdoc3.html
 ; interrupt reference: https://en.wikipedia.org/wiki/BIOS_interrupt_call#Interrupt_table
-%define LF 10
-%define CR 13
-%define EOF 0
+%define LF 10 ; line feed
+%define CR 13 ; cariage return
+%define NULL 0 ; null terminator
 %define BG_COLOR 9 ; royal blue
 
 [ORG 0x7c00]
@@ -10,7 +10,7 @@
    mov ds, ax ; move ax to the datasegment register
    cld        ; clear direction flag, that decides in which direction
               ; e.g. loadsb should load the memory (++ as usual or -- for
-			  ; reading backwards)
+              ; reading backwards)
 
    mov bl, BG_COLOR  ; store the background color in the proper register
                      ; for int10h with function 0xB
@@ -18,7 +18,7 @@
 
    mov si, msg  ; store msg pointer into the proper register for the
                 ; int10h with function 0xE
-   call print
+   call println
 
 hang:
    jmp hang
@@ -31,8 +31,8 @@ set_bg_color:
 
 print:
    lodsb           ; load next byte from 
-   or al, al       ; check if null terminator reached
-   jz done         ; finish print method if null terminator reached
+   or al, al       ; set zero flag if null terminator has been reached
+   jz done         ; finish print method if null terminator reached (zero flag reached)
    mov bl, 1
    mov ah, 0x0E    ; select 'Write character in TTY Mode' function of int 10h 
    int 0x10        ; call interrupt
@@ -40,7 +40,14 @@ print:
 done:
    ret
 
-msg   db 'Hello Raphael!', CR, LF, EOF
+println:
+   call print
+   mov si, newline
+   call print
+   ret
+
+msg     db 'Hello Raphael!', NULL
+newline db CR, LF
  
    times 510-($-$$) db 0
    db 0x55
